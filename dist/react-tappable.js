209d34db1a05466a5457a7f9bb7a88fe5ff1f484
Jed Watson <jed.watson@me.com>
2015-02-19 23:18:25 +1100

    Updating Build

--- dist/react-tappable.js
+++ dist/react-tappable.js
@@ -15,15 +15,7 @@ function getTouchProps(touch) {
 	};
 }
 
-function extend(target, source) {
-	if (!source || Object.prototype.toString.call(source) !== '[object Object]') return target;
-	for (var key in source) {
-		if (source.hasOwnProperty(key)) {
-			target[key] = source[key];
-		}
-	}
-	return target;
-}
+var extend = require('react/lib/Object.assign');
 
 /**
  * Tappable Mixin
@@ -171,7 +163,7 @@ function extend(target, source) {
 		this.endTouch(event);
 	},
 	
-	endTouch: function() {
+	endTouch: function(event) {
 		this.cancelPressDetection();
 		this.props.onTouchEnd && this.props.onTouchEnd(event);
 		this._initialTouch = null;
@@ -284,8 +276,7 @@ var component = React.createClass({
 		}
 		
 		var style = {};
-		extend(style, this.touchStyles());
-		extend(style, this.props.style);
+		extend(style, this.touchStyles(), this.props.style);
 		
 		return React.createElement(this.props.component, {
 			style: style,
@@ -307,5 +298,52 @@ component.Mixin = Mixin;
 module.exports = component;
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"react/lib/Object.assign":2}],2:[function(require,module,exports){
+/**
+ * Copyright 2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule Object.assign
+ */
+
+// https://people.mozilla.org/~jorendorff/es6-draft.html#sec-object.assign
+
+function assign(target, sources) {
+  if (target == null) {
+    throw new TypeError('Object.assign target cannot be null or undefined');
+  }
+
+  var to = Object(target);
+  var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+  for (var nextIndex = 1; nextIndex < arguments.length; nextIndex++) {
+    var nextSource = arguments[nextIndex];
+    if (nextSource == null) {
+      continue;
+    }
+
+    var from = Object(nextSource);
+
+    // We don't currently support accessors nor proxies. Therefore this
+    // copy cannot throw. If we ever supported this then we must handle
+    // exceptions and side-effects. We don't support symbols so they won't
+    // be transferred.
+
+    for (var key in from) {
+      if (hasOwnProperty.call(from, key)) {
+        to[key] = from[key];
+      }
+    }
+  }
+
+  return to;
+};
+
+module.exports = assign;
+
 },{}]},{},[1])(1)
 });
