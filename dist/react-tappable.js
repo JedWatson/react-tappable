47839bdebe7c0971fe09493e09bf2131b5b355e2
Jed Watson <jed.watson@me.com>
2015-07-31 22:38:38 +1000

    Updating build

--- dist/react-tappable.js
+++ dist/react-tappable.js
@@ -1,7 +1,18 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Tappable = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+var TappableMixin = require('./TappableMixin');
+var getComponent = require('./getComponent');
+var touchStyles = require('./touchStyles');
+
+var Component = getComponent([TappableMixin]);
+
+module.exports = Component;
+module.exports.touchStyles = touchStyles;
+module.exports.Mixin = TappableMixin;
+
+},{"./TappableMixin":2,"./getComponent":3,"./touchStyles":4}],2:[function(require,module,exports){
+'use strict';
 
 var React = (window.React);
 
@@ -14,37 +25,6 @@ function getTouchProps(touch) {
 		clientY: touch.clientY
 	};
 }
-
-function isDataOrAriaProp(key) {
-	return key.indexOf('data-') === 0 || key.indexOf('aria-') === 0;
-}
-
-function getPinchProps(touches) {
-	return {
-		touches: Array.prototype.map.call(touches, function copyTouch(touch) {
-			return { identifier: touch.identifier, pageX: touch.pageX, pageY: touch.pageY };
-		}),
-		center: { x: (touches[0].pageX + touches[1].pageX) / 2, y: (touches[0].pageY + touches[1].pageY) / 2 },
-		angle: Math.atan() * (touches[1].pageY - touches[0].pageY) / (touches[1].pageX - touches[0].pageX) * 180 / Math.PI,
-		distance: Math.sqrt(Math.pow(Math.abs(touches[1].pageX - touches[0].pageX), 2) + Math.pow(Math.abs(touches[1].pageY - touches[0].pageY), 2))
-	};
-}
-
-var TOUCH_STYLES = {
-	WebkitTapHighlightColor: 'rgba(0,0,0,0)',
-	WebkitTouchCallout: 'none',
-	WebkitUserSelect: 'none',
-	KhtmlUserSelect: 'none',
-	MozUserSelect: 'none',
-	msUserSelect: 'none',
-	userSelect: 'none',
-	cursor: 'pointer'
-};
-
-/**
- * Tappable Mixin
- * ==============
- */
 
 var Mixin = {
 	propTypes: {
@@ -63,11 +43,7 @@ var Mixin = {
 		onMouseDown: React.PropTypes.func, // pass-through mouse event
 		onMouseUp: React.PropTypes.func, // pass-through mouse event
 		onMouseMove: React.PropTypes.func, // pass-through mouse event
-		onMouseOut: React.PropTypes.func, // pass-through mouse event
-
-		onPinchStart: React.PropTypes.func, // fires when a pinch gesture is started
-		onPinchMove: React.PropTypes.func, // fires on every touch-move when a pinch action is active
-		onPinchEnd: React.PropTypes.func // fires when a pinch action ends
+		onMouseOut: React.PropTypes.func // pass-through mouse event
 	},
 
 	getDefaultProps: function getDefaultProps() {
@@ -107,7 +83,7 @@ var Mixin = {
 			this.initScrollDetection();
 			this.initPressDetection(event, this.endTouch);
 			this._activeTimeout = setTimeout(this.makeActive, this.props.activeDelay);
-		} else if ((this.props.onPinchStart || this.props.onPinchMove || this.props.onPinchEnd) && event.touches.length === 2) {
+		} else if (this.onPinchStart && (this.props.onPinchStart || this.props.onPinchMove || this.props.onPinchEnd) && event.touches.length === 2) {
 			this.onPinchStart(event);
 		}
 	},
@@ -123,87 +99,6 @@ var Mixin = {
 	clearActiveTimeout: function clearActiveTimeout() {
 		clearTimeout(this._activeTimeout);
 		this._activeTimeout = false;
-	},
-
-	onPinchStart: function onPinchStart(event) {
-		// in case the two touches didn't start exactly at the same time
-		if (this._initialTouch) {
-			this.endTouch();
-		}
-		var touches = event.touches;
-		this._initialPinch = getPinchProps(touches);
-		this._initialPinch = _extends(this._initialPinch, {
-			displacement: { x: 0, y: 0 },
-			displacementVelocity: { x: 0, y: 0 },
-			rotation: 0,
-			rotationVelocity: 0,
-			zoom: 1,
-			zoomVelocity: 0,
-			time: Date.now()
-		});
-		this._lastPinch = this._initialPinch;
-		this.props.onPinchStart && this.props.onPinchStart(this._initialPinch, event);
-	},
-
-	onPinchMove: function onPinchMove(event) {
-		if (this._initialTouch) {
-			this.endTouch();
-		}
-		var touches = event.touches;
-		if (touches.length !== 2) {
-			return this.onPinchEnd(event) // bail out before disaster
-			;
-		}
-
-		var currentPinch = touches[0].identifier === this._initialPinch.touches[0].identifier && touches[1].identifier === this._initialPinch.touches[1].identifier ? getPinchProps(touches) // the touches are in the correct order
-		: touches[1].identifier === this._initialPinch.touches[0].identifier && touches[0].identifier === this._initialPinch.touches[1].identifier ? getPinchProps(touches.reverse()) // the touches have somehow changed order
-		: getPinchProps(touches); // something is wrong, but we still have two touch-points, so we try not to fail
-
-		currentPinch.displacement = {
-			x: currentPinch.center.x - this._initialPinch.center.x,
-			y: currentPinch.center.y - this._initialPinch.center.y
-		};
-
-		currentPinch.time = Date.now();
-		var timeSinceLastPinch = currentPinch.time - this._lastPinch.time;
-
-		currentPinch.displacementVelocity = {
-			x: (currentPinch.displacement.x - this._lastPinch.displacement.x) / timeSinceLastPinch,
-			y: (currentPinch.displacement.y - this._lastPinch.displacement.y) / timeSinceLastPinch
-		};
-
-		currentPinch.rotation = currentPinch.angle - this._initialPinch.angle;
-		currentPinch.rotationVelocity = currentPinch.rotation - this._lastPinch.rotation / timeSinceLastPinch;
-
-		currentPinch.zoom = currentPinch.distance / this._initialPinch.distance;
-		currentPinch.zoomVelocity = (currentPinch.zoom - this._lastPinch.zoom) / timeSinceLastPinch;
-
-		this.props.onPinchMove && this.props.onPinchMove(currentPinch, event);
-
-		this._lastPinch = currentPinch;
-	},
-
-	onPinchEnd: function onPinchEnd(event) {
-		// TODO use helper to order touches by identifier and use actual values on touchEnd.
-		var currentPinch = _extends({}, this._lastPinch);
-		currentPinch.time = Date.now();
-
-		if (currentPinch.time - this._lastPinch.time > 16) {
-			currentPinch.displacementVelocity = 0;
-			currentPinch.rotationVelocity = 0;
-			currentPinch.zoomVelocity = 0;
-		}
-
-		this.props.onPinchEnd && this.props.onPinchEnd(currentPinch, event);
-
-		this._initialPinch = this._lastPinch = null;
-
-		// If one finger is still on screen, it should start a new touch event for swiping etc
-		// But it should never fire an onTap or onPress event.
-		// Since there is no support swipes yet, this should be disregarded for now
-		// if (event.touches.length === 1) {
-		// 	this.onTouchStart(event);
-		// }
 	},
 
 	initScrollDetection: function initScrollDetection() {
@@ -282,7 +177,7 @@ var Mixin = {
 					});
 				}
 			}
-		} else if (this._initialPinch && event.touches.length === 2) {
+		} else if (this._initialPinch && event.touches.length === 2 && this.onPinchMove) {
 			this.onPinchMove(event);
 			event.preventDefault();
 		}
@@ -310,7 +205,7 @@ var Mixin = {
 				};
 			}
 			this.endTouch(event, afterEndTouch);
-		} else if (this._initialPinch && event.touches.length + event.changedTouches.length === 2) {
+		} else if (this.onPinchEnd && this._initialPinch && event.touches.length + event.changedTouches.length === 2) {
 			this.onPinchEnd(event);
 			event.preventDefault();
 		}
@@ -382,10 +277,6 @@ var Mixin = {
 		this._mouseDown = false;
 	},
 
-	touchStyles: function touchStyles() {
-		return TOUCH_STYLES;
-	},
-
 	handlers: function handlers() {
 		return {
 			onTouchStart: this.onTouchStart,
@@ -399,69 +290,91 @@ var Mixin = {
 	}
 };
 
+module.exports = Mixin;
+
+},{}],3:[function(require,module,exports){
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var React = (window.React);
+var touchStyles = require('./touchStyles');
+
 /**
  * Tappable Component
  * ==================
  */
+module.exports = function (mixins) {
+	return React.createClass({
+		displayName: 'Tappable',
 
-var Component = React.createClass({
+		mixins: mixins,
 
-	displayName: 'Tappable',
+		propTypes: {
+			component: React.PropTypes.any, // component to create
+			className: React.PropTypes.string, // optional className
+			classBase: React.PropTypes.string, // base for generated classNames
+			style: React.PropTypes.object, // additional style properties for the component
+			disabled: React.PropTypes.bool // only applies to buttons
+		},
 
-	mixins: [Mixin],
+		getDefaultProps: function getDefaultProps() {
+			return {
+				component: 'span',
+				classBase: 'Tappable'
+			};
+		},
 
-	propTypes: {
-		component: React.PropTypes.any, // component to create
-		className: React.PropTypes.string, // optional className
-		classBase: React.PropTypes.string, // base for generated classNames
-		style: React.PropTypes.object, // additional style properties for the component
-		disabled: React.PropTypes.bool // only applies to buttons
-	},
+		render: function render() {
+			var props = this.props;
+			var className = props.classBase + (this.state.isActive ? '-active' : '-inactive');
 
-	getDefaultProps: function getDefaultProps() {
-		return {
-			component: 'span',
-			classBase: 'Tappable'
-		};
-	},
+			if (props.className) {
+				className += ' ' + props.className;
+			}
 
-	render: function render() {
-		var props = this.props;
-		var className = props.classBase + (this.state.isActive ? '-active' : '-inactive');
+			var style = {};
+			_extends(style, touchStyles, props.style);
 
-		if (props.className) {
-			className += ' ' + props.className;
+			var newComponentProps = _extends({}, props, {
+				style: style,
+				className: className,
+				disabled: props.disabled,
+				handlers: this.handlers
+			}, this.handlers());
+
+			delete newComponentProps.onTap;
+			delete newComponentProps.onPress;
+			delete newComponentProps.onPinchStart;
+			delete newComponentProps.onPinchMove;
+			delete newComponentProps.onPinchEnd;
+			delete newComponentProps.moveThreshold;
+			delete newComponentProps.pressDelay;
+			delete newComponentProps.pressMoveThreshold;
+			delete newComponentProps.preventDefault;
+			delete newComponentProps.stopPropagation;
+			delete newComponentProps.component;
+
+			return React.createElement(props.component, newComponentProps, props.children);
 		}
+	});
+};
 
-		var style = {};
-		_extends(style, this.touchStyles(), props.style);
+},{"./touchStyles":4}],4:[function(require,module,exports){
+'use strict';
 
-		var newComponentProps = _extends({}, props, {
-			style: style,
-			className: className,
-			disabled: props.disabled,
-			handlers: this.handlers
-		}, this.handlers());
+var touchStyles = {
+  WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+  WebkitTouchCallout: 'none',
+  WebkitUserSelect: 'none',
+  KhtmlUserSelect: 'none',
+  MozUserSelect: 'none',
+  msUserSelect: 'none',
+  userSelect: 'none',
+  cursor: 'pointer'
+};
 
-		delete newComponentProps.onTap;
-		delete newComponentProps.onPress;
-		delete newComponentProps.onPinchStart;
-		delete newComponentProps.onPinchMove;
-		delete newComponentProps.onPinchEnd;
-		delete newComponentProps.moveThreshold;
-		delete newComponentProps.pressDelay;
-		delete newComponentProps.pressMoveThreshold;
-		delete newComponentProps.preventDefault;
-		delete newComponentProps.stopPropagation;
-		delete newComponentProps.component;
-
-		return React.createElement(props.component, newComponentProps, props.children);
-	}
-});
-
-Component.Mixin = Mixin;
-Component.touchStyles = TOUCH_STYLES;
-module.exports = Component;
+module.exports = touchStyles;
 
 },{}]},{},[1])(1)
 });
