1e8751d99ef6b303d099e97b7f8ad69bbb28fd7c
Jed Watson <jed.watson@me.com>
2015-07-31 22:24:13 +1000

    Exporting touchStyles from Punchable and Tappable

--- src/Pinchable.js
+++ src/Pinchable.js
@@ -1,7 +1,9 @@
 var PinchableBaseMixin = require('./PinchableBaseMixin');
 var PinchableMixin = require('./PinchableMixin');
 var getComponent = require('./getComponent');
+var touchStyles = require('./touchStyles');
 
 var Component = getComponent([PinchableBaseMixin, PinchableMixin]);
 
 module.exports = Component;
+module.exports.touchStyles = touchStyles;
--- src/Tappable.js
+++ src/Tappable.js
@@ -1,6 +1,8 @@
 var TappableMixin = require('./TappableMixin');
 var getComponent = require('./getComponent');
+var touchStyles = require('./touchStyles');
 
 var Component = getComponent([TappableMixin]);
 
 module.exports = Component;
+module.exports.touchStyles = touchStyles;
