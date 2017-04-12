b5643690498ff79c62d8af191ce9a8ef52bd6166
Niels van Midden <niels.vanmidden@gmail.com>
2017-04-12 17:08:11 +0200

    tabify

--- src/getComponent.js
+++ src/getComponent.js
@@ -7,66 +7,66 @@ var touchStyles = require('./touchStyles');
  * ==================
  */
 module.exports = function (mixins) {
-  return React.createClass({
-    displayName: 'Tappable',
+	return React.createClass({
+		displayName: 'Tappable',
 
-    mixins: mixins,
+		mixins: mixins,
 
-    propTypes: {
-      component: PropTypes.any,           // component to create
-      className: PropTypes.string,        // optional className
-      classBase: PropTypes.string,        // base for generated classNames
-      classes: PropTypes.object,          // object containing the active and inactive class names
-      style: PropTypes.object,            // additional style properties for the component
-      disabled: PropTypes.bool            // only applies to buttons
-    },
+		propTypes: {
+			component: PropTypes.any,           // component to create
+			className: PropTypes.string,        // optional className
+			classBase: PropTypes.string,        // base for generated classNames
+			classes: PropTypes.object,          // object containing the active and inactive class names
+			style: PropTypes.object,            // additional style properties for the component
+			disabled: PropTypes.bool            // only applies to buttons
+		},
 
-    getDefaultProps: function () {
-      return {
-        component: 'span',
-        classBase: 'Tappable'
-      };
-    },
+		getDefaultProps: function () {
+			return {
+				component: 'span',
+				classBase: 'Tappable'
+			};
+		},
 
-    render: function () {
-      var props = this.props;
-      var className = props.classBase + (this.state.isActive ? '-active' : '-inactive');
+		render: function () {
+			var props = this.props;
+			var className = props.classBase + (this.state.isActive ? '-active' : '-inactive');
 
-      if (props.className) {
-        className += ' ' + props.className;
-      }
+			if (props.className) {
+				className += ' ' + props.className;
+			}
 
-      if (props.classes) {
-        className += ' ' + (this.state.isActive ? props.classes.active : props.classes.inactive);
-      }
+			if (props.classes) {
+				className += ' ' + (this.state.isActive ? props.classes.active : props.classes.inactive);
+			}
 
-      var style = {};
-      Object.assign(style, touchStyles, props.style);
+			var style = {};
+			Object.assign(style, touchStyles, props.style);
 
-      var newComponentProps = Object.assign({}, props, {
-        style: style,
-        className: className,
-        disabled: props.disabled,
-        handlers: this.handlers
-      }, this.handlers());
+			var newComponentProps = Object.assign({}, props, {
+				style: style,
+				className: className,
+				disabled: props.disabled,
+				handlers: this.handlers
+			}, this.handlers());
 
-      delete newComponentProps.activeDelay;
-      delete newComponentProps.classBase;
-      delete newComponentProps.classes;
-      delete newComponentProps.handlers;
-      delete newComponentProps.onTap;
-      delete newComponentProps.onPress;
-      delete newComponentProps.onPinchStart;
-      delete newComponentProps.onPinchMove;
-      delete newComponentProps.onPinchEnd;
-      delete newComponentProps.moveThreshold;
-      delete newComponentProps.pressDelay;
-      delete newComponentProps.pressMoveThreshold;
-      delete newComponentProps.preventDefault;
-      delete newComponentProps.stopPropagation;
-      delete newComponentProps.component;
+			delete newComponentProps.activeDelay;
+			delete newComponentProps.classBase;
+			delete newComponentProps.classes;
+			delete newComponentProps.handlers;
+			delete newComponentProps.onTap;
+			delete newComponentProps.onPress;
+			delete newComponentProps.onPinchStart;
+			delete newComponentProps.onPinchMove;
+			delete newComponentProps.onPinchEnd;
+			delete newComponentProps.moveThreshold;
+			delete newComponentProps.pressDelay;
+			delete newComponentProps.pressMoveThreshold;
+			delete newComponentProps.preventDefault;
+			delete newComponentProps.stopPropagation;
+			delete newComponentProps.component;
 
-      return React.createElement(props.component, newComponentProps, props.children);
-    }
-  });
+			return React.createElement(props.component, newComponentProps, props.children);
+		}
+	});
 };
